9439b8d3a8de963aee1aa70b8905c8cfbac22150
dudms100 <qye215@gmail.com>
2024-05-31 17:56:16 +0900

    학원에서수업

--- src/ex07string/test.java
+++ src/ex07string/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1.학원에서push");
+		
+		System.out.println("3.학워에서수업");
 
 	}
 
